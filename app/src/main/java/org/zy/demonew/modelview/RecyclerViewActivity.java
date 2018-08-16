c33ea9f1ab2f33e015a59556acae6d7b606915fc
William Yue <zhangyuede230@163.com>
2018-08-16 16:51:52 +0800

    recyclerViewActivity add restore logic

--- app/src/main/java/org/zy/demonew/modelview/RecyclerViewActivity.java
+++ app/src/main/java/org/zy/demonew/modelview/RecyclerViewActivity.java
@@ -13,6 +13,8 @@ import org.zy.demonew.modelview.model.User;
 import java.util.ArrayList;
 
 public class RecyclerViewActivity extends AppCompatActivity {
+    private final static String INFO_USERLIST = "user_info_list";
+
     private RecyclerView recyclerView;
     private ArrayList<User> users = new ArrayList<>();
 
@@ -21,34 +23,44 @@ public class RecyclerViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recyclerview);
 
-        User user0 = new User("Zhang0", "Yue");
-        User user1 = new User("Zhang1", "Yue");
-        User user2 = new User("Zhang2", "Yue");
-        User user3 = new User("Zhang3", "Yue");
-        User user4 = new User("Zhang4", "Yue");
-        User user5 = new User("Zhang5", "Yue");
-        User user6 = new User("Zhang6", "Yue");
-        User user7 = new User("Zhang7", "Yue");
-        User user8 = new User("Zhang8", "Yue");
-        User user9 = new User("Zhang9", "Yue");
-        User user10 = new User("Zhang10", "Yue");
-        User user11 = new User("Zhang11", "Yue");
-        users.add(user0);
-        users.add(user1);
-        users.add(user2);
-        users.add(user3);
-        users.add(user4);
-        users.add(user5);
-        users.add(user6);
-        users.add(user7);
-        users.add(user8);
-        users.add(user9);
-        users.add(user10);
-        users.add(user11);
+        if (savedInstanceState == null) {
+            User user0 = new User("Zhang0", "Yue");
+            User user1 = new User("Zhang1", "Yue");
+            User user2 = new User("Zhang2", "Yue");
+            User user3 = new User("Zhang3", "Yue");
+            User user4 = new User("Zhang4", "Yue");
+            User user5 = new User("Zhang5", "Yue");
+            User user6 = new User("Zhang6", "Yue");
+            User user7 = new User("Zhang7", "Yue");
+            User user8 = new User("Zhang8", "Yue");
+            User user9 = new User("Zhang9", "Yue");
+            User user10 = new User("Zhang10", "Yue");
+            User user11 = new User("Zhang11", "Yue");
+            users.add(user0);
+            users.add(user1);
+            users.add(user2);
+            users.add(user3);
+            users.add(user4);
+            users.add(user5);
+            users.add(user6);
+            users.add(user7);
+            users.add(user8);
+            users.add(user9);
+            users.add(user10);
+            users.add(user11);
+        } else {
+            users = savedInstanceState.getParcelableArrayList(INFO_USERLIST);
+        }
 
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this,
                 LinearLayoutManager.VERTICAL, false));
         recyclerView.setAdapter(new MyRecyclerAdapter(this, users));
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelableArrayList(INFO_USERLIST, users);
     }
 }
--- app/src/main/java/org/zy/demonew/modelview/model/User.java
+++ app/src/main/java/org/zy/demonew/modelview/model/User.java
@@ -1,6 +1,9 @@
 package org.zy.demonew.modelview.model;
 
-public class User {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class User implements Parcelable {
     public final String firstName;
     public final String lastName;
 
@@ -8,4 +11,32 @@ public class User {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.firstName);
+        dest.writeString(this.lastName);
+    }
+
+    protected User(Parcel in) {
+        this.firstName = in.readString();
+        this.lastName = in.readString();
+    }
+
+    public static final Parcelable.Creator<User> CREATOR = new Parcelable.Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel source) {
+            return new User(source);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
 }
